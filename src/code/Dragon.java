f468abcadbb41f257d1ad8703321760a1a447804
David Martinez <Dmartinez21@my.bcit.ca>
2025-09-20 14:37:32 -0700

    pushed everything

--- src/code/Dragon.java
+++ src/code/Dragon.java
@@ -1,6 +1,38 @@
-public class Dragon extends RuntimeException {
-    public Dragon(String message)
+public class Dragon extends Creature
+{
+
+    public static final int MINIMUM_FIRE_POWER = 1;
+    public static final int MAXIMUM_FIRE_POWER = 100;
+
+    private final int firePower;
+
+    public Dragon(String name, Date dateOfBirth, int health, int firePower)
     {
-        super(message);
+        super(name, dateOfBirth, health);
+
+        checkFirePower(firePower);
+        this.firePower = firePower;
     }
+
+    private void checkFirePower(int firePower)
+    {
+        if (firePower < MINIMUM_FIRE_POWER || firePower > MAXIMUM_FIRE_POWER)
+        {
+            throw new IllegalArgumentException("Fire power must be between "
+                    + MINIMUM_FIRE_POWER
+                    + " and "
+                    + MAXIMUM_FIRE_POWER + ".");
+        }
+    }
+
+    @Override
+    public String getDetails()
+    {
+        return super.getDetails() + ", Fire Power: " + firePower;
+    }
+
+    public void breatheFire() {
+
+    }
+
 }
