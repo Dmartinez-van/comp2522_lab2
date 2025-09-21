d7e00846fe3b1bc6489da3f630e94c557594d36a
David Martinez <Dmartinez21@my.bcit.ca>
2025-09-21 13:47:44 -0700

    Fix some magic numbers. Change LowRageException to unchecked RuntimeException.

--- src/code/Orc.java
+++ src/code/Orc.java
@@ -1,15 +1,15 @@
+/**
+ * Represents an Orc, a type of Creature with a rage attribute.
+ * Provides methods to access details and perform berserk attacks.
+ *
+ * @author Daniel Do, David Martinez
+ * @version 1.0
+ */
 public class Orc extends Creature
 {
-    /**
-     * Represents an Orc, a type of Creature with a rage attribute.
-     * Provides methods to access details and perform berserk attacks.
-     *
-     * @author Daniel Do, David Martinez
-     * @version 1.0
-     */
-
-    private static final int RAGE_INCREASE_POINTS = 5;
-    private static final int MIN_RAGE_POINTS = 5;
+    private static final int BERSERK_INCREASE_RAGE_POINTS = 5;
+    private static final int BERSERK_RAGE_COST = 5;
+    private static final int MIN_RAGE_POINTS = 0;
     private static final int RAGE_THRESHOLD_POINTS = 20;
     private static final int DAMAGE_NORMAL_HP_POINTS = 15;
     private static final int DAMAGE_DOUBLE_HP_POINTS = 30;
@@ -25,7 +25,10 @@ public class Orc extends Creature
      * @param rage        the initial rage of the orc, must be non-negative
      * @throws IllegalArgumentException if any parameter is invalid
      */
-    public Orc(final String name, final Date dateOfBirth, final int health, final int rage)
+    public Orc(final String name,
+               final Date dateOfBirth,
+               final int health,
+               final int rage)
     {
         super(name, dateOfBirth, health);
         checkRage(rage);
@@ -34,9 +37,10 @@ public class Orc extends Creature
 
     private void checkRage(int rage)
     {
-        if (rage < 0)
+        if (rage < MIN_RAGE_POINTS)
         {
-            throw new IllegalArgumentException("Rage must be non-negative.");
+            throw new IllegalArgumentException("Rage must be greater than " +
+                    MIN_RAGE_POINTS);
         }
     }
 
@@ -48,35 +52,47 @@ public class Orc extends Creature
     @Override
     public final String getDetails()
     {
-        return super.getDetails() + ", Rage: " + rage;
+        return super.getDetails() + "Rage: " + rage + "\n";
     }
 
     /**
      * Performs a berserk attack on the specified target creature.
      * <p>
-     * This method increases the orc's rage by {@value RAGE_INCREASE_POINTS} points.
-     * If the resulting rage is less than {@value MIN_RAGE_POINTS}, a LowRageException is thrown.
-     * If the resulting rage exceeds {@value RAGE_THRESHOLD_POINTS}, the orc deals double damage
+     * This method increases the orc's rage by
+     * {@value BERSERK_INCREASE_RAGE_POINTS} points.
+     * Berserk costs {@value BERSERK_RAGE_COST}, if current rage is lower a
+     * LowRageException is thrown.
+     * If the resulting rage exceeds {@value RAGE_THRESHOLD_POINTS}, the orc
+     * deals double damage
      * ({@value DAMAGE_DOUBLE_HP_POINTS} health points) to the target creature.
-     * Otherwise, the orc deals normal damage ({@value DAMAGE_NORMAL_HP_POINTS} health points).
+     * Otherwise, the orc deals normal damage
+     * ({@value DAMAGE_NORMAL_HP_POINTS} health points).
      * <p>
-     * The rage value is immutable in this implementation; to allow rage to increase, remove the final modifier.
+     * The rage value is immutable in this implementation; to allow rage to
+     * increase, remove the final modifier.
      *
-     * @throws LowRageException         if the resulting rage is less than {@value MIN_RAGE_POINTS}
+     * @throws LowRageException         if the resulting rage is less than
+     *                                  {@value MIN_RAGE_POINTS}
      * @throws IllegalArgumentException if targetCreature is null
      */
     public final int berserk() throws LowRageException
     {
+        final int newRage;
 
-        final int newRage = this.rage + 5;
-        if (newRage < 5)
+        newRage = rage + BERSERK_INCREASE_RAGE_POINTS;
+
+        if (newRage < MIN_RAGE_POINTS)
         {
             throw new LowRageException("Rage is too low to go berserk.");
         }
-        if (newRage > 20)
+
+        rage = newRage;
+
+        if (rage > RAGE_THRESHOLD_POINTS)
         {
             return DAMAGE_DOUBLE_HP_POINTS;
-        } else
+        }
+        else
         {
             return DAMAGE_NORMAL_HP_POINTS;
         }
